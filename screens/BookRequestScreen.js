f1c743b2dbbb538687c7bca6ad4ba012733356a4
Mumal Swami <66372736+mumal123@users.noreply.github.com>
2020-11-12 09:10:35 +0530

    Update BookRequestScreen.js

--- screens/BookRequestScreen.js
+++ screens/BookRequestScreen.js
@@ -10,21 +10,116 @@ export default class BookRequestScreen extends React.Component {
         this.state={
             userId:firebase.auth().currentUser.email,
             bookName:"",
-            reasonToRequest:""
+            reasonToRequest:"",
+            requestId:'',
+            isBookRequestActive:'',
+            requestedBookName:'',
+            bookStatus:'',
+            userdocId:'',
+            docId:'',
+            requestId:''
         }
     }
     createUniqueId() {
         return Math.random().toString(36).substring(7)
         
     }
-    addRequest=(bookName,reasonToRequest)=>{
+    getBookRequest=()=>{
+        var bookRequest=db.collection('requested_books').where('user_id','==',this.state.userId).get()
+        .then(snapshot=>{
+            snapshot.forEach(doc=>{
+                if(doc.data().book_status === 'recieved'){
+                    this.setState({
+                        requestId:doc.data().request_id,
+                        requestedBookName:doc.data().book_name,
+                        docId:doc.id
+                    })
+                }
+            })
+        })
+    }
+    getIsBookRequestActive(){
+        db.collection('users').where('email_id','==',this.state.userId)
+        .onSnapshot(querySnapshot=>{
+            querySnapshot.forEach(doc=>{
+                this.setState({
+                    isBookRequestActiive:doc.data().IsBookRequestActive,
+                    userdocId:doc.id
+
+                })
+            })
+        })
+    }
+    updateBookRequestStatus=()=>{
+db.collection('requested_books').doc(this.state.docId).update({
+    book_status:'recieved'
+})
+db.collection('users').where('email_id','==',userId).get()
+.then(snapshot=>{
+    snapshot.forEach(doc=>{
+        db.collection('users').doc(doc.id).update({
+            isBookRequestActiive:false
+        })
+    })
+})
+    }
+    sendNotification=()=>{
+db.collection('users').where('email_id','=='.this.state.userId).get()
+.then(snapshot=>{
+    snapshot.forEach(doc=>{
+        var name=doc.data().first_name
+        var lastName=doc.data().last_name
+    })
+})
+db.collection('all_notifications').where('request_id','==',this.state.requestId).get()
+.then(snapshot=>{
+    snapshot.forEach(doc=>{
+        var donorId=doc.data().donor_id
+        var bookName=doc.data().book_name
+    })
+    })
+    db.collection('all_notifications').add({
+        'targeted_user_id':donorId,
+        'message':name+' '+lastName+' recieved the book '+bookName,
+        'book_name':bookName
+
+    })
+}
+recievedBooks=(bookName)=>{
+var userId=this.state.userId
+var requestId=this.state.requestId
+db.collection('received_books').add({
+    'user_id':userId,
+    'book_name':bookName,
+    'request_id':requestId,
+    'book_status':bookStatus
+})
+}
+
+    componentDidMount(){
+        this.getBookRequest()
+        this.getIsBookRequestActive()
+    }
+    addRequest=async(bookName,reasonToRequest)=>{
         var userId=this.state.userId
         var randomRequest=this.createUniqueId()
         db.collection("requested_books").add({
             "user_id":userId,
             "book_name":bookName,
             "reason_to_request":reasonToRequest,
-            "request_id":randomRequestId
+            "request_id":randomRequestId,
+            'book_status':'requested',
+            'date':firesbase.firestore.FieldValue.serverTimestamp()
+
+        })
+        await this.getBookRequest()
+        db.collection('users').where('email_id','==',userId).get()
+        .then(snapshot=>{
+            snapshot.forEach(doc=>{
+                db.collection('users').doc(doc.id).update({
+                    isBookRequestActive:true
+                })
+            })
         })
         this.setState({
             bookName: "",
@@ -33,7 +128,32 @@ export default class BookRequestScreen extends React.Component {
         return Alert.alert("Book Requested Successfully")
     }
     render() {
-       
+       if(this.state.isBookRequestActiive === true){
+return(
+    <View style={{flex:1 ,justifyContent:'center'}}>
+<View style={{borderColor:'orange',borderWidth:2,justifyContent:'center',alignItems:'center' ,padding:20}}>
+<Text>Book Name</Text>
+<Text>{this.state.requestedBookName}</Text>
+</View>
+<View style={{borderColor:'orange',borderWidth:2,justifyContent:'center',alignItems:'center' ,padding:20}}>
+<Text>Book Status</Text>
+<Text>{this.state.bookStatus}</Text>
+
+</View>
+<TouchableOpacity style={{borderColor:'orange',borderWidth:1,backgroundColor:'orange',width:300,alignItems:'center',height:50}} 
+onPress={()=>{
+    this.sendNotification()
+    this.updateBookRequestStatus()
+    this.recievedBooks(this.state.requestedBookName)
+}}
+
+>
+<Text>I have recieved the books</Text>
+</TouchableOpacity>
+    </View>
+)
+       }
+       else{
         return (
             <View style={{ flex: 1 }}>
                 <MyHeader title="Book Request" />
@@ -73,6 +193,7 @@ export default class BookRequestScreen extends React.Component {
             
         )
     }
+}
 }
 const styles=StyleSheet.create({
     keyBoardStyle : {
